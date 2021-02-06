d6a365595d3518cba951ebfdd63754a8deb8309c
Gaurav Jain <gaurav.jain1@evaluserve.com>
2021-02-06 21:49:08 +0530

    fix debug

--- softwars/settings.py
+++ softwars/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bh%kn$eum+2llu@(bf24#r=e+^(ce=gojn23drjk#f1reu$yc0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
